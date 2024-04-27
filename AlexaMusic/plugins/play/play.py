dd71112d75834ef2e03be0f9d5e5b649cfde9c3d
Salamqq <163534782+Salamqq@users.noreply.github.com>
2024-04-27 08:24:54 +0300

    Update play.py

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -39,13 +39,10 @@ from strings import get_command
 from AlexaMusic.utils.database import is_served_user
 
 # Command
-PLAY_COMMAND = get_command("PLAY_COMMAND")
+PLAY_COMMAND = get_command("شغل")
 
 
-@app.on_message(command(["فيديو","شغل","تشغيل"])
-    & filters.group
-    & ~BANNED_USERS
-)
+@app.on_message(filters.command(PLAY_COMMAND) & filters.group & ~BANNED_USERS)
 @PlayWrapper
 async def play_commnd(
     client,
