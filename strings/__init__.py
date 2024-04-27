4a5363dd4b03525b1c09060347e590c2d5d7c08b
Salamqq <163534782+Salamqq@users.noreply.github.com>
2024-04-27 12:35:01 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -26,18 +26,18 @@ def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./strings"):
+for filename in os.listdir(r".strings"):
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
+            open(r".strings/" + filename, encoding="utf8")
         )
 
 
-for filename in os.listdir(r"./strings/langs/"):
+for filename in os.listdir(r".strings langs"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+            open(r". strings langs en.yml", encoding="utf8")
         )
     if filename.endswith(".yml"):
         language_name = filename[:-4]
